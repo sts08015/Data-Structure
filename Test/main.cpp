f5002e46a78d6d31b7b9e6ae8580a61333b83f05
FisMA <31784008+sts08015@users.noreply.github.com>
2019-01-10 21:44:16 +0900

    ha

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     char a[30];
     scanf("%s",a);
-    printf("%s",a);
+    printf("%s\n",a);
+
+    char * x;
+    int n;
+    scanf("%d",&n);
+    x = (char *)malloc(n*sizeof(char));
+    scanf("%s",x);
+    printf("%s",x);
+    free(x);
     return 0;
 }
